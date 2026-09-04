8341b5369928c2388429ef5fc0c7f354a3cf82a0
srirajaguru <srirajaguru.m@care.ac.in>
2026-09-04 10:00:59 +0530

    Update the style of login and home page module

--- config/config/settings.py
+++ config/config/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Email
